89185dedf01c09c0862539559c4613f312d1d092
César Salazar <72630184+cesarcash@users.noreply.github.com>
2022-05-27 18:20:44 -0500

    index

---
+++ js/current-weather.js
@@ -0,0 +1,10 @@
+import weather from '../data/current-weather.js';
+
+function configCurrentWeather(weather){
+    
+}
+
+export default function currentWeather(){
+    configCurrentWeather(weather);
+    console.log(weather);
+}
