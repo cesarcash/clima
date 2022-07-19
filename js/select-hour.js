734d4197ba4c1656af2bfb7b4f8e3406249e072e
César Salazar <72630184+cesarcash@users.noreply.github.com>
2022-07-18 23:22:09 -0500

    lunes

--- js/select-hour.js
+++ js/select-hour.js
@@ -1,27 +1,29 @@
 
 export function selectItemHour(){
 
-    const $tabPanel = document.querySelectorAll('.tabPanel')
+    // const $tabPanel = document.querySelectorAll('.tabPanel')
 
-    $tabPanel.forEach(($item,index) => {
-        const $tabItem = $item.querySelectorAll('.dayWeather-item')
+    // $tabPanel.forEach(($item,index) => {
+        const $tabItem = document.querySelectorAll('.dayWeather-item')
         $tabItem.forEach(($itemHour,indexHour) => {
-            $itemHour.addEventListener('click', function(e){
-
-                const $itemSelected = this
-                console.log($itemSelected)
-                const $itemActive = document.querySelector('.is-selected')
-                console.log($itemActive)
-            
-            })
+            $itemHour.addEventListener('click', handleSelectItemHour)
         })
-    })
+    // })
 
 
 }
 
 function handleSelectItemHour(){
 
-    console.log(this)
+    const $itemSelected = this
+    const $containerItem = $itemSelected.parentElement
+
+    const $itemActive = $containerItem.querySelector('.is-selected')
+    $itemActive.classList.remove('is-selected')
+    $itemSelected.classList.add('is-selected')
+
+    const summaryId = $itemSelected.id
+    const $summarySelected = document.querySelector(`[data-id="${summaryId}"]`)
+    $summarySelected.style.display = 'grid'
 
 }
